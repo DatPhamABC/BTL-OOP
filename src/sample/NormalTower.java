e57d989f06966e7fb32371d5a353671a3f5e5f32
DatPhamABC <56540868+DatPhamABC@users.noreply.github.com>
2019-11-08 16:17:25 +0700

    Add files via upload

--- src/sample/NormalTower.java
+++ src/sample/NormalTower.java
@@ -1,7 +1,11 @@
 package sample;
 
-public class NormalTower extends  Tower {
-    public NormalTower(sample.image image, int x, int y, Bullet bullet) {
-        super(image, x, y, bullet);
+import javafx.scene.image.Image;
+
+public class NormalTower extends Tower {
+    public NormalTower(int x_pos, int y_pos){
+        super(new Image("file:images\\tower1.png"), 5, 200, 2);
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
     }
 }
