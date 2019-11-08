5fa3ed1b27d355f1667586bcad22dccabdaaece3
DatPhamABC <56540868+DatPhamABC@users.noreply.github.com>
2019-11-09 00:18:11 +0700

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,12 +5,14 @@ import javafx.animation.KeyFrame;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
@@ -30,53 +32,54 @@ public class Main extends Application {
         gameField.rendermap(primaryStage,0,0);
         GameStage gameStage = new GameStage(13);
         gameStage.loadArrayEnemy("arrEnemy.txt");
-        
-        
+
+
+
+        Bullet bullet2 = new Bullet(new image("file:images\\bullet.png"),100,100,100);
+        Tower tower = new Tower(null,bullet2);
+
+//        Circle path = new Circle(72*4.5, 72*4.5, 72*6);
+//        path.setFill(null);
+//        path.setStroke(Color.RED);
+//        Config.group.getChildren().add(path);
+//        primaryStage.setScene(Config.scene);
+//
+//        Circle path1 = new Circle(72*4.5, 72*4.5, 72*4);
+//        path1.setFill(null);
+//        path1.setStroke(Color.BLUE);
+//        Config.group.getChildren().add(path1);
+//        Config.pane.getChildren().add(Config.group);
+//        primaryStage.setScene(Config.scene);
+//        primaryStage.show();
+
+
         Config.pane.setOnMousePressed(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
                 if (event.isPrimaryButtonDown()){
                     Config.x_pos = ((int)event.getX()/Config.sizeimageMap)*Config.sizeimageMap;
                     Config.y_pos = ((int)event.getY()/Config.sizeimageMap)*Config.sizeimageMap;
-                    HighDamage tower = new HighDamage(Config.x_pos, Config.y_pos);
-                    tower.shoot(primaryStage, tankerEnemy1);
-                    tower.towerBuild(primaryStage);
+                    if (GameField.arrmap[(Config.y_pos / Config.sizeimageMap)][(Config.x_pos / Config.sizeimageMap)].equals("2")) {
+                        NormalTower tower = new NormalTower(Config.x_pos, Config.y_pos, bullet2);
+                        tower.towerBuild(primaryStage);
+                    }
                 }
             }
         });
-        
-        
-
-        Bullet bullet2 = new Bullet(new image("file:images\\bullet.png"),100,100,100);
-        Tower tower = new Tower(null,bullet2);
-
-        Circle path = new Circle(72*4.5, 72*4.5, 72*6);
-        path.setFill(null);
-        path.setStroke(Color.RED);
-        Config.group.getChildren().add(path);
-        primaryStage.setScene(Config.scene);
-
-        Circle path1 = new Circle(72*4.5, 72*4.5, 72*4);
-        path1.setFill(null);
-        path1.setStroke(Color.BLUE);
-        Config.group.getChildren().add(path1);
-        Config.pane.getChildren().add(Config.group);
-        primaryStage.setScene(Config.scene);
-        primaryStage.show();
 
         Stack<Enemy> newStack =gameStage.getStackEnemy();
         Stack<Enemy> newStack1 =gameStage.getStackEnemy();
-        //Stack<String> stringStack1 = new Stack<>();
-        //stringStack1.push("right");
-       // NormalEnemy normalEnemy = new NormalEnemy();
-       // normalEnemy.Run(primaryStage,gameStage.x,gameStage.y,stringStack1);
+//        Stack<String> stringStack1 = new Stack<>();
+//        stringStack1.push("right");
+//        NormalEnemy normalEnemy = new NormalEnemy();
+//        normalEnemy.Run(primaryStage,gameStage.x,gameStage.y,stringStack1);
 
-        image image2 = new image("file:images\\SpinerTower.png");
-        image2.show(primaryStage,72-15,72+30);
-        image image = new image("file:images\\MGTower.png");
-        image.show(primaryStage,72*3-15,72*3);
-        image image1 = new image("file:images\\NormalTower.png");
-        image1.show(primaryStage,72*4,72*1);
+//        image image2 = new image("file:images\\SpinerTower.png");
+//        image2.show(primaryStage,72-15,72+30);
+//        image image = new image("file:images\\MGTower.png");
+//        image.show(primaryStage,72*3-15,72*3);
+//        image image1 = new image("file:images\\NormalTower.png");
+//        image1.show(primaryStage,72*4,72*1);
         Timeline timeline = new
                 Timeline(new KeyFrame(Duration.millis(2000),
                 (evt)->{
@@ -93,9 +96,9 @@ public class Main extends Application {
 //                    {
 //                        tower.setEnemyTarget(false);
 //                    }
-                    image2.show(primaryStage,72-15,72+30);
-                    image.show(primaryStage,72*3-15,72*3);
-                    image1.show(primaryStage,72*4,72*1);
+//                    image2.show(primaryStage,72-15,72+30);
+//                    image.show(primaryStage,72*3-15,72*3);
+//                    image1.show(primaryStage,72*4,72*1);
 
 
                 }
@@ -112,9 +115,9 @@ public class Main extends Application {
                                     newStack.pop().Run(primaryStage,gameStage.x,gameStage.y,stringStack);
                                 }*/
 
-                        image2.show(primaryStage,72-15,72+30);
-                        image.show(primaryStage,72*3-15,72*3);
-                        image1.show(primaryStage,72*4,72*1);
+//                        image2.show(primaryStage,72-15,72+30);
+//                        image.show(primaryStage,72*3-15,72*3);
+//                        image1.show(primaryStage,72*4,72*1);
 
                 }
         ));
