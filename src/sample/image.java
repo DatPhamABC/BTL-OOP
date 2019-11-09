c138f6ab56d73f1f6c4f40d44a71f74b0d1e3439
DatPhamABC <56540868+DatPhamABC@users.noreply.github.com>
2019-11-09 22:59:19 +0700

    Update image.java

--- src/sample/image.java
+++ src/sample/image.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
  class image {
     private Image image;
     private ImageView imageView;
+    private String url;
 
 
     /*public image(Image image) {
@@ -27,18 +28,22 @@ import javafx.stage.Stage;
     public image(String url)
     {
         image = new Image(url);
+        this.url = url;
     }
     public image(String url, boolean backgroundLoading)
     {
         image = new Image(url);
+        this.url = url;
     }
     public image(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth)
     {
         image = new Image(url,requestedWidth,requestedHeight,preserveRatio,smooth);
+        this.url = url;
     }
     public image(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth,boolean backgroundLoading)
     {
         image = new Image(url,requestedWidth,requestedHeight,preserveRatio,smooth,backgroundLoading);
+        this.url = url;
     }
     //get && set
 
@@ -80,83 +85,9 @@ import javafx.stage.Stage;
         imageView.imageProperty().set(null);
     }
 
-}
-/*
-package sample;
-import java.io.InputStream;
-
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
- class image {
-    private Image image;
-    private ImageView imageView;
-
-
-    /*public image(Image image) {
-        this.image = image;
+    public boolean equals(image image){
+        if (!this.url.equals(image.url)) return false;
+        return true;
     }
 
-     // Constructor
-     public image()
-     {
-     }
-     public image(String url)
-     {
-         image = new Image(url);
-     }
-     public image(String url, boolean backgroundLoading)
-     {
-         image = new Image(url);
-     }
-     public image(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth)
-     {
-         image = new Image(url,requestedWidth,requestedHeight,preserveRatio,smooth);
-     }
-     public image(String url, double requestedWidth, double requestedHeight, boolean preserveRatio, boolean smooth,boolean backgroundLoading)
-     {
-         image = new Image(url,requestedWidth,requestedHeight,preserveRatio,smooth,backgroundLoading);
-     }
-     //get && set
-
-     public Image getImage() {
-         return image;
-     }
-
-     public void setImage(Image image) {
-         this.image = image;
-     }
-
-     public ImageView getImageView() {
-         return imageView;
-     }
-
-     public void setImageView(ImageView imageView) {
-         this.imageView = imageView;
-     }
-
-     // method
-     void show(Stage stage,int x,int y)
-     {
-         imageView = new ImageView(image);
-         changePosition(x,y);
-         Config.pane.getChildren().add(imageView);
-         stage.setScene(Config.scene);
-         stage.show();
-
-     }
-     void changePosition(/* position  pos int x,int y)
-     {
-         imageView.setX(x);
-         imageView.setY(y);
-     }
-
- }
-
- */
+}
